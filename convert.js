a4fc782320affdf3637a2ca043677bae7781b5e8
Thomas Pummer <thomas.pummer@univie.ac.at>
2017-08-01 11:44:40 +0200

    added mode selection

--- convert.js
+++ convert.js
@@ -4,17 +4,22 @@
  
 $(document).ready(function(){
   $('#converter').submit(function(e) {
-      e.preventDefault();
-      
-	  var input = $('#in').val();
-	  //TODO insert mode selction
-	  var result = viz(input);
-	  
-	  $('#out').val(result);
+    e.preventDefault();
+    
+    var algo = $('.convert-mode option:selected').val();
+    var input = $('#in').val();
+    
+    switch (algo){
+      case 'pgng2e' : var result = pgng2e(input); break;
+      case 'pgne2g' : var result = pgne2g(input); break;
+      default: break;
+    }
+    
+    $('#out').val(result);
   });
 });
 
-function viz(str){
+function pgng2e(str){
    var isComment = 0;
    var isHeader  = 0;
    var result = str;
@@ -38,7 +43,35 @@ function viz(str){
          default: break;
        }
      }
-   } 	
+   }   
+   return result;
+}
+
+function pgne2g(str){
+   var isComment = 0;
+   var isHeader  = 0;
+   var result = str;
+   
+   for (var i = 0, len = str.length; i < len; i++) {
+
+     if ( isComment || isHeader )
+     {
+        if ( isComment && str[i]=='}' )  isComment = 0;
+        if ( isHeader  && str[i]==']' )  isHeader  = 0;
+     }
+     else
+     {
+       switch ( str[i] ) {
+         case '{' : isComment = 1;  break;
+         case '[' : isHeader  = 1;  break;
+         case 'Q' : result = setCharAt(result,i,'D');  break;
+         case 'R' : result = setCharAt(result,i,'T');  break;
+         case 'B' : result = setCharAt(result,i,'L');  break;
+         case 'N' : result = setCharAt(result,i,'S');  break;
+         default: break;
+       }
+     }
+   }   
    return result;
 }
 
